46058e8718ac021d612ea64e11b33e42e25764be
ImSana <aasursyakov@gmail.com>
2023-10-18 08:54:03 +0300

    отредактировал фикстуру

--- conftest.py
+++ conftest.py
@@ -5,13 +5,13 @@ from main import BooksCollector
 @pytest.fixture
 def collector():
     collector = BooksCollector()
-    collector.add_new_book()
-    collector.add_new_book()
-    collector.set_book_genre()
-    collector.get_book_genre()
-    collector.add_book_in_favorites()
-    collector.get_books_for_children()
-    collector.get_books_with_specific_genre()
-    collector.delete_book_from_favorites()
-    collector.get_list_of_favorites_books()
+    collector.add_new_book('Гордость и предубеждение и зомби')
+    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+    collector.get_book_genre('Гордость и предубеждение и зомби')
+    collector.get_books_with_specific_genre('Ужасы')
+    collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+    collector.get_list_of_favorites_books('Гордость и предубеждение и зомби')
+    collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
+
     return collector
