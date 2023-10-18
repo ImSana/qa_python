4efe38e9d26b7d9b94c2e9d5e90068deefbe0ac9
ImSana <aasursyakov@gmail.com>
2023-10-18 11:13:36 +0300

    Создал test_set_book_genre

--- conftest.py
+++ conftest.py
@@ -11,7 +11,6 @@ def collector():
     collector.get_book_genre('Гордость и предубеждение и зомби')
     collector.get_books_with_specific_genre('Ужасы')
     collector.add_book_in_favorites('Гордость и предубеждение и зомби')
-    collector.get_list_of_favorites_books('Гордость и предубеждение и зомби')
     collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
 
     return collector
